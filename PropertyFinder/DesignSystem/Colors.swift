5a83999c409397181b44c2754b57946fa4008f7d
Paula Hasstenteufel <paula@hasstenteufel.com>
2024-06-28 16:59:24 +0200

    Moving Assets to Resources folder, adding custom brand colors and design system main colors and sizes.

---
+++ PropertyFinder/DesignSystem/Colors.swift
@@ -0,0 +1,27 @@
+//
+//  Colors.swift
+//  PropertyFinder
+//
+//  Created by Paula Hasstenteufel on 2024-06-28.
+//
+
+import SwiftUI
+
+extension Color {
+    // Brand
+    static let brandDark = Color(red: 0.449, green: 0.615, blue: 0.481, opacity: 1)
+    static let brandMedium = Color(red: 0.835, green: 0.863, blue: 0.808, opacity: 1)
+    static let brandLight = Color(red: 0.835, green: 0.863, blue: 0.808, opacity: 1)
+    
+    // Neutral
+    static let strong = Color(red: 0.071, green: 0.071, blue: 0.071, opacity: 1)
+    static let medium = Color(red: 0.447, green: 0.463, blue: 0.494, opacity: 1)
+    static let soft = Color(red: 0.839, green: 0.843, blue: 0.855, opacity: 1)
+    
+    static let background = Color.white
+    
+    // Price Feedback
+    static let lowPrice = Color(red: 0.039, green: 0.533, blue: 0.329, opacity: 1)
+    static let highPrice = Color(red: 0.922, green: 0.027, blue: 0.267, opacity: 1)
+}
+
---
+++ PropertyFinder/DesignSystem/Dimensions.swift
@@ -0,0 +1,16 @@
+//
+//  Dimensions.swift
+//  PropertyFinder
+//
+//  Created by Paula Hasstenteufel on 2024-06-28.
+//
+
+import Foundation
+
+extension CGFloat {
+    static let large = 18.0
+    static let primary = 16.0
+    static let small = 10.0
+    
+    static let frame = 2.0
+}
